122ef138363f06192ecfce05c26e036975579aec
CarlosLucena <carlos_lucena@hotmail.com>
2024-12-05 00:21:50 -0500

    Task 4

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -39,7 +39,11 @@ public_users.get('/author/:author',function (req, res) {
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let rslt = Object.fromEntries(Object.entries(books).filter(book => book[1]["title"] == req.params.title))
+  if (rslt) {
+    return res.status(200).json(rslt);
+  }
+  return res.status(404);
 });
 
 //  Get book review
