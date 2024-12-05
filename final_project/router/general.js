fe28aef3504236340fe272af463eb8bd1dc033f6
CarlosLucena <carlos_lucena@hotmail.com>
2024-12-05 00:16:22 -0500

    Task 2 & 3

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -19,13 +19,21 @@ public_users.get('/',function (req, res) {
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const book = books[req.params.isbn]
+  if (book) {
+    return res.status(200).json(book);
+  }
+  return res.status(404);
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let rslt = Object.fromEntries(Object.entries(books).filter(book => book[1]["author"] == req.params.author))
+  if (rslt) {
+    return res.status(200).json(rslt);
+  }
+  return res.status(404);
 });
 
 // Get all books based on title
